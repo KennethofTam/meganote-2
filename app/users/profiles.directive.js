ae4bff8f0c2f207dfdce23c121f332ba925be0ed
Kenneth Tam <ktam@purdue.edu>
2016-07-11 14:20:58 -0400

    Add link to profile and back to notes

--- app/users/profiles.directive.js
+++ app/users/profiles.directive.js
@@ -50,6 +50,11 @@
 
                   <input type="submit" name="commit" value="Save Changes"
                     class="btn btn-default">
+                  <span class="login">
+                    <a ui-sref="notes.form({ noteId: undefined })">
+                      Back to my notes
+                    </a>
+                  </span>
                 </form>
               </div>
             </div>
